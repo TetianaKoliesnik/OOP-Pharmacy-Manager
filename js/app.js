95b063d525380aa6bf33341289ee17646820601c
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-05-15 17:34:53 +0200

    select elements from dom

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("test");
+//selecting elements from the DOM
+const productName = document.querySelector(".product-name");
+const productType = document.querySelector(".product-type");
+const manufacturer = document.querySelector(".manufacturer");
+const expirationDate = document.querySelector(".expiration-date");
+const quantity = document.querySelector(".quantity");
+const tabletsDosageOption = document.querySelector(".tablets-dose-mg");
+const liquidDosageOption = document.querySelector(".liquid-dose-ml");
+const creamUsageOption = document.querySelector(".creams-usage-per-day");
