96c645df54b5573f96a8354dee9ca0d935f74d08
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-05-15 17:36:04 +0200

    add eventlisteners and classes

--- js/app.js
+++ js/app.js
@@ -7,3 +7,68 @@ const quantity = document.querySelector(".quantity");
 const tabletsDosageOption = document.querySelector(".tablets-dose-mg");
 const liquidDosageOption = document.querySelector(".liquid-dose-ml");
 const creamUsageOption = document.querySelector(".creams-usage-per-day");
+
+//adding event listeners
+
+productType.addEventListener("change", () => {
+	if (productType.value === "tablets") {
+	  liquidDosageOption.setAttribute("disabled", "");
+	  tabletsDosageOption.removeAttribute("disabled");
+	  creamUsageOption.setAttribute("disabled", "");
+	} else if (productType.value === "liquids") {
+	  tabletsDosageOption.setAttribute("disabled", "");
+	  creamUsageOption.setAttribute("disabled", "");
+	  liquidDosageOption.removeAttribute("disabled");
+	} else if (productType.value === "creams") {
+	  liquidDosageOption.setAttribute("disabled", "");
+	  tabletsDosageOption.setAttribute("disabled", "");
+	  creamUsageOption.removeAttribute("disabled");
+	} else {
+	  liquidDosageOption.setAttribute("disabled", "");
+	  tabletsDosageOption.setAttribute("disabled", "");
+	  creamUsageOption.setAttribute("disabled", "");
+	}
+  });
+  
+  //declaring classes
+  
+  class Product {
+	constructor(
+	  productName,
+	  productType,
+	  manufacturer,
+	  expirationDate,
+	  quantity
+	) {
+	  this.productName = productName;
+	  this.productType = productType;
+	  this.manufacturer = manufacturer;
+	  this.expirationDate = expirationDate;
+	  this.quantity = quantity;
+	  this.ID = Date.now();
+	}
+  }
+  
+  class Tablets extends Product {
+	constructor(productName, productType, manufacturer, expirationDate, quantity, tabletsDosageOption){
+	  super(productName, productType, manufacturer, expirationDate, quantity)
+	  this.tabletsDosageOption = tabletsDosageOption;
+	  this.ID = Date.now();
+	}
+  }
+  
+  class Liquids extends Product {
+	  constructor(productName, productType, manufacturer, expirationDate, quantity, liquidDosageOption){
+		super(productName, productType, manufacturer, expirationDate, quantity)
+		this.liquidDosageOption = liquidDosageOption;
+		this.ID = Date.now();
+	  }
+	}
+  
+	class Creams extends Product {
+	  constructor(productName, productType, manufacturer, expirationDate, quantity, creamUsageOption){
+		super(productName, productType, manufacturer, expirationDate, quantity)
+		this.creamUsageOption = creamUsageOption;
+		this.ID = Date.now();
+	  }
+	}
