b6450b20ebf8e34b63816eb9f2c242b266488293
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-05-27 14:02:20 +0200

    add more logic, change dosageOption order in constructor

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+//create empty arrays so the newly created objects can be stored there
+const tabletMeds = [];
+const liquidMeds = [];
+const creamMeds = [];
+
 //selecting elements from the DOM
+const addNewProductForm = document.querySelector(".add-new-product-form");
 const productName = document.querySelector(".product-name");
 const productType = document.querySelector(".product-type");
 const manufacturer = document.querySelector(".manufacturer");
@@ -9,66 +15,137 @@ const liquidDosageOption = document.querySelector(".liquid-dose-ml");
 const creamUsageOption = document.querySelector(".creams-usage-per-day");
 
 //adding event listeners
-
 productType.addEventListener("change", () => {
-	if (productType.value === "tablets") {
-	  liquidDosageOption.setAttribute("disabled", "");
-	  tabletsDosageOption.removeAttribute("disabled");
-	  creamUsageOption.setAttribute("disabled", "");
-	} else if (productType.value === "liquids") {
-	  tabletsDosageOption.setAttribute("disabled", "");
-	  creamUsageOption.setAttribute("disabled", "");
-	  liquidDosageOption.removeAttribute("disabled");
-	} else if (productType.value === "creams") {
-	  liquidDosageOption.setAttribute("disabled", "");
-	  tabletsDosageOption.setAttribute("disabled", "");
-	  creamUsageOption.removeAttribute("disabled");
-	} else {
-	  liquidDosageOption.setAttribute("disabled", "");
-	  tabletsDosageOption.setAttribute("disabled", "");
-	  creamUsageOption.setAttribute("disabled", "");
-	}
-  });
-  
-  //declaring classes
-  
-  class Product {
+  if (productType.value === "tablets") {
+    liquidDosageOption.setAttribute("disabled", "");
+    tabletsDosageOption.removeAttribute("disabled");
+    creamUsageOption.setAttribute("disabled", "");
+  } else if (productType.value === "liquids") {
+    tabletsDosageOption.setAttribute("disabled", "");
+    creamUsageOption.setAttribute("disabled", "");
+    liquidDosageOption.removeAttribute("disabled");
+  } else if (productType.value === "creams") {
+    liquidDosageOption.setAttribute("disabled", "");
+    tabletsDosageOption.setAttribute("disabled", "");
+    creamUsageOption.removeAttribute("disabled");
+  } else {
+    liquidDosageOption.setAttribute("disabled", "");
+    tabletsDosageOption.setAttribute("disabled", "");
+    creamUsageOption.setAttribute("disabled", "");
+  }
+});
+
+addNewProductForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newProduct;
+  if (productType.value === "tablets") {
+    newProduct = new Tablets(
+      productName.value,
+      productType.value,
+      tabletsDosageOption.value,
+      manufacturer.value,
+      expirationDate.value,
+      quantity.value
+    );
+  } else if (productType.value === "liquids") {
+    newProduct = new Liquids(
+      productName.value,
+      productType.value,
+      liquidDosageOption.value,
+      manufacturer.value,
+      expirationDate.value,
+      quantity.value
+    );
+  } else if (productType.value === "creams") {
+    newProduct = new Creams(
+      productName.value,
+      productType.value,
+      creamUsageOption.value,
+      manufacturer.value,
+      expirationDate.value,
+      quantity.value
+    );
+  }
+  Product.addProduct(newProduct);
+  //reset here
+  console.log(newProduct);
+  console.log(tabletMeds);
+  console.log(liquidMeds);
+  console.log(creamMeds);
+});
+
+//declaring classes
+class Product {
+  constructor(
+    productName,
+    productType,
+    manufacturer,
+    expirationDate,
+    quantity
+  ) {
+    this.productName = productName;
+    this.productType = productType;
+    this.manufacturer = manufacturer;
+    this.expirationDate = expirationDate;
+    this.quantity = quantity;
+    this.ID = Date.now();
+  }
+
+  static addProduct(meds) {
+    if (meds.productType === "tablets") {
+      tabletMeds.push(meds);
+    } else if (meds.productType === "liquids") {
+      liquidMeds.push(meds);
+    } else if (meds.productType === "creams") {
+      creamMeds.push(meds);
+    } //maybe needs changing because i have productType === "*" in index too
+  }
+}
+
+class Tablets extends Product {
 	constructor(
 	  productName,
 	  productType,
+	  tabletsDosageOption,
 	  manufacturer,
 	  expirationDate,
-	  quantity
+	  quantity,
+	  
 	) {
-	  this.productName = productName;
-	  this.productType = productType;
-	  this.manufacturer = manufacturer;
-	  this.expirationDate = expirationDate;
-	  this.quantity = quantity;
-	  this.ID = Date.now();
-	}
-  }
-  
-  class Tablets extends Product {
-	constructor(productName, productType, manufacturer, expirationDate, quantity, tabletsDosageOption){
-	  super(productName, productType, manufacturer, expirationDate, quantity)
+	  super(productName, productType, manufacturer, expirationDate, quantity);
 	  this.tabletsDosageOption = tabletsDosageOption;
 	  this.ID = Date.now();
 	}
   }
   
   class Liquids extends Product {
-	  constructor(productName, productType, manufacturer, expirationDate, quantity, liquidDosageOption){
-		super(productName, productType, manufacturer, expirationDate, quantity)
-		this.liquidDosageOption = liquidDosageOption;
-		this.ID = Date.now();
-	  }
+	constructor(
+	  productName,
+	  productType,
+	  liquidDosageOption,
+	  manufacturer,
+	  expirationDate,
+	  quantity,
+	  
+	) {
+	  super(productName, productType, manufacturer, expirationDate, quantity);
+	  this.liquidDosageOption = liquidDosageOption;
+	  this.ID = Date.now();
 	}
+  }
   
-	class Creams extends Product {
-	  constructor(productName, productType, manufacturer, expirationDate, quantity, creamUsageOption){
-		super(productName, productType, manufacturer, expirationDate, quantity)
-		this.creamUsageOption = creamUsageOption;
-		this.ID = Date.now();
-	  }
+  class Creams extends Product {
+	constructor(
+	  productName,
+	  productType,
+	  creamUsageOption,
+	  manufacturer,
+	  expirationDate,
+	  quantity,
+	  
+	) {
+	  super(productName, productType, manufacturer, expirationDate, quantity);
+	  this.creamUsageOption = creamUsageOption;
+	  this.ID = Date.now();
 	}
+  }
