183a98104f4491983be090f0e960863be7f4e63c
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-05-28 14:05:11 +0200

    begin on rendering logic

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //create empty arrays so the newly created objects can be stored there
+const allMeds = [];
 const tabletMeds = [];
 const liquidMeds = [];
 const creamMeds = [];
@@ -13,6 +14,21 @@ const quantity = document.querySelector(".quantity");
 const tabletsDosageOption = document.querySelector(".tablets-dose-mg");
 const liquidDosageOption = document.querySelector(".liquid-dose-ml");
 const creamUsageOption = document.querySelector(".creams-usage-per-day");
+
+const allMedsUl = document.querySelector(".all-products-list");
+const tabletMedsUl = document.querySelector(".tablets-list");
+const liquidMedsUl = document.querySelector(".liquids-list");
+const creamMedsUl = document.querySelector(".creams-list");
+
+const displayAllMedsContainer = document.querySelector(".display-all-products");
+const displayTabletMedsContainer = document.querySelector(".display-tablets");
+const displayLiquidMedsContainer = document.querySelector(".display-liquids");
+const displayCreamMedsContainer = document.querySelector(".display-creams");
+
+const renderAllMedsButton = document.querySelector(".render-all-button");
+const renderTabletMedsButton = document.querySelector(".render-tablets-button");
+const renderLiquidMedsButton = document.querySelector(".render-liquids-button");
+const renderCreamMedsButton = document.querySelector(".render-creams-button");
 
 //adding event listeners
 productType.addEventListener("change", () => {
@@ -72,7 +88,14 @@ addNewProductForm.addEventListener("submit", (e) => {
   console.log(tabletMeds);
   console.log(liquidMeds);
   console.log(creamMeds);
+  console.log(allMeds);
 });
+
+//adding event listeners
+renderAllMedsButton.addEventListener("click", ()=>{
+	UI.activeTab = "all";
+	UI.renderMeds(allMeds)
+})
 
 //declaring classes
 class Product {
@@ -93,59 +116,96 @@ class Product {
 
   static addProduct(meds) {
     if (meds.productType === "tablets") {
-      tabletMeds.push(meds);
+      tabletMeds.push(meds), allMeds.push(meds);
     } else if (meds.productType === "liquids") {
-      liquidMeds.push(meds);
+      liquidMeds.push(meds), allMeds.push(meds);
     } else if (meds.productType === "creams") {
-      creamMeds.push(meds);
+      creamMeds.push(meds), allMeds.push(meds);
     } //maybe needs changing because i have productType === "*" in index too
   }
 }
 
 class Tablets extends Product {
-	constructor(
-	  productName,
-	  productType,
-	  tabletsDosageOption,
-	  manufacturer,
-	  expirationDate,
-	  quantity,
-	  
-	) {
-	  super(productName, productType, manufacturer, expirationDate, quantity);
-	  this.tabletsDosageOption = tabletsDosageOption;
-	  this.ID = Date.now();
-	}
+  constructor(
+    productName,
+    productType,
+    tabletsDosageOption,
+    manufacturer,
+    expirationDate,
+    quantity
+  ) {
+    super(productName, productType, manufacturer, expirationDate, quantity);
+    this.tabletsDosageOption = tabletsDosageOption;
+    this.ID = Date.now();
   }
-  
-  class Liquids extends Product {
-	constructor(
-	  productName,
-	  productType,
-	  liquidDosageOption,
-	  manufacturer,
-	  expirationDate,
-	  quantity,
-	  
-	) {
-	  super(productName, productType, manufacturer, expirationDate, quantity);
-	  this.liquidDosageOption = liquidDosageOption;
-	  this.ID = Date.now();
-	}
+}
+
+class Liquids extends Product {
+  constructor(
+    productName,
+    productType,
+    liquidDosageOption,
+    manufacturer,
+    expirationDate,
+    quantity
+  ) {
+    super(productName, productType, manufacturer, expirationDate, quantity);
+    this.liquidDosageOption = liquidDosageOption;
+    this.ID = Date.now();
   }
-  
-  class Creams extends Product {
-	constructor(
-	  productName,
-	  productType,
-	  creamUsageOption,
-	  manufacturer,
-	  expirationDate,
-	  quantity,
-	  
-	) {
-	  super(productName, productType, manufacturer, expirationDate, quantity);
-	  this.creamUsageOption = creamUsageOption;
-	  this.ID = Date.now();
-	}
+}
+
+class Creams extends Product {
+  constructor(
+    productName,
+    productType,
+    creamUsageOption,
+    manufacturer,
+    expirationDate,
+    quantity
+  ) {
+    super(productName, productType, manufacturer, expirationDate, quantity);
+    this.creamUsageOption = creamUsageOption;
+    this.ID = Date.now();
   }
+}
+
+//declaring UI class
+
+class UI {
+  static activeTab = "all";
+  static renderMeds(allMeds) {
+    displayAllMedsContainer.style.display = "block";
+    displayTabletMedsContainer.style.display = "none";
+    displayLiquidMedsContainer.style.display = "none";
+    displayCreamMedsContainer.style.display = "none";
+	allMedsUl.textContent = " ";
+    if (UI.activeTab === "all") {
+      allMeds.forEach((med) => {
+        const liRow = document.createElement("li");
+        const renderedProductName = document.createElement("span");
+        const renderedProductType = document.createElement("span");
+        const renderedManufacturer = document.createElement("span");
+        const renderedExpirationDate = document.createElement("span");
+        const renderedQuantity = document.createElement("span");
+        const deleteButtonContainer = document.createElement("span");
+        const deleteButton = document.createElement("button");
+
+		renderedProductName.textContent = med.productName;
+		renderedProductType.textContent = med.productType;
+		renderedManufacturer.textContent = med.manufacturer;
+		renderedExpirationDate.textContent = med.expirationDate;
+		renderedQuantity.textContent = med.quantity;
+		deleteButton.textContent = 'Delete ❌';
+
+		liRow.classList.add("display-all-row");
+		deleteButton.classList.add('delete-button');
+
+
+		allMedsUl.append(liRow);
+		liRow.append(renderedProductName, renderedProductType, renderedManufacturer, renderedExpirationDate, renderedQuantity, deleteButtonContainer);
+		deleteButtonContainer.append(deleteButton);
+      });
+    }
+  }
+}
