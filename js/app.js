fb6a86b7caec886bef9914ef1679ebd9799fef34
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-05-28 16:13:02 +0200

    add render tablets logic.Isn't working

--- js/app.js
+++ js/app.js
@@ -8,12 +8,12 @@ const creamMeds = [];
 const addNewProductForm = document.querySelector(".add-new-product-form");
 const productName = document.querySelector(".product-name");
 const productType = document.querySelector(".product-type");
-const manufacturer = document.querySelector(".manufacturer");
-const expirationDate = document.querySelector(".expiration-date");
-const quantity = document.querySelector(".quantity");
 const tabletsDosageOption = document.querySelector(".tablets-dose-mg");
 const liquidDosageOption = document.querySelector(".liquid-dose-ml");
 const creamUsageOption = document.querySelector(".creams-usage-per-day");
+const manufacturer = document.querySelector(".manufacturer");
+const expirationDate = document.querySelector(".expiration-date");
+const quantity = document.querySelector(".quantity");
 
 const allMedsUl = document.querySelector(".all-products-list");
 const tabletMedsUl = document.querySelector(".tablets-list");
@@ -83,7 +83,8 @@ addNewProductForm.addEventListener("submit", (e) => {
     );
   }
   Product.addProduct(newProduct);
-  //reset here
+  
+  
   console.log(newProduct);
   console.log(tabletMeds);
   console.log(liquidMeds);
@@ -92,10 +93,16 @@ addNewProductForm.addEventListener("submit", (e) => {
 });
 
 //adding event listeners
-renderAllMedsButton.addEventListener("click", ()=>{
-	UI.activeTab = "all";
-	UI.renderMeds(allMeds)
-})
+renderAllMedsButton.addEventListener("click", () => {
+  UI.activeTab = "all";
+  UI.renderMeds(allMeds);
+});
+
+renderTabletMedsButton.addEventListener("click", ()=> {
+  UI.activeTab = "tablets";
+  UI.renderTabletMeds(tabletMeds);
+  console.log(tabletMedsUl);
+});
 
 //declaring classes
 class Product {
@@ -121,7 +128,7 @@ class Product {
       liquidMeds.push(meds), allMeds.push(meds);
     } else if (meds.productType === "creams") {
       creamMeds.push(meds), allMeds.push(meds);
-    } //maybe needs changing because i have productType === "*" in index too
+    } //maybe needs changing because i have productType === "*" in index.html too
   }
 }
 
@@ -171,7 +178,6 @@ class Creams extends Product {
 }
 
 //declaring UI class
-
 class UI {
   static activeTab = "all";
   static renderMeds(allMeds) {
@@ -179,7 +185,7 @@ class UI {
     displayTabletMedsContainer.style.display = "none";
     displayLiquidMedsContainer.style.display = "none";
     displayCreamMedsContainer.style.display = "none";
-	allMedsUl.textContent = " ";
+    allMedsUl.textContent = " "; //DOESNT SEEM TO WORK
     if (UI.activeTab === "all") {
       allMeds.forEach((med) => {
         const liRow = document.createElement("li");
@@ -191,20 +197,68 @@ class UI {
         const deleteButtonContainer = document.createElement("span");
         const deleteButton = document.createElement("button");
 
-		renderedProductName.textContent = med.productName;
-		renderedProductType.textContent = med.productType;
-		renderedManufacturer.textContent = med.manufacturer;
-		renderedExpirationDate.textContent = med.expirationDate;
-		renderedQuantity.textContent = med.quantity;
-		deleteButton.textContent = 'Delete ❌';
+        renderedProductName.textContent = med.productName;
+        renderedProductType.textContent = med.productType;
+        renderedManufacturer.textContent = med.manufacturer;
+        renderedExpirationDate.textContent = med.expirationDate;
+        renderedQuantity.textContent = med.quantity;
+        deleteButton.textContent = "Delete ❌";
 
-		liRow.classList.add("display-all-row");
-		deleteButton.classList.add('delete-button');
+        liRow.classList.add("display-all-row");
+        deleteButton.classList.add("delete-button");
 
+        allMedsUl.append(liRow);
+        liRow.append(
+          renderedProductName,
+          renderedProductType,
+          renderedManufacturer,
+          renderedExpirationDate,
+          renderedQuantity,
+          deleteButtonContainer
+        );
+        deleteButtonContainer.append(deleteButton);
+      });
+    }
+  }
 
-		allMedsUl.append(liRow);
-		liRow.append(renderedProductName, renderedProductType, renderedManufacturer, renderedExpirationDate, renderedQuantity, deleteButtonContainer);
-		deleteButtonContainer.append(deleteButton);
+  static renderTabletMeds(tabletMeds) {
+    tabletMedsUl.textContent = "";
+    displayAllMedsContainer.style.display = "none";
+    displayTabletMedsContainer.style.display = "block";
+    displayLiquidMedsContainer.style.display = "none";
+    displayCreamMedsContainer.style.display = "none";
+
+    if (UI.activeTab === "tablets") {
+      tabletMeds.forEach((tabMed) => {
+        const liRow = document.createElement("li");
+        const renderedProductName = document.createElement("span");
+        const renderedProductType = document.createElement("span");
+        const renderedTabletsDosageOption = document.createElement("span");
+        const renderedManufacturer = document.createElement("span");
+        const renderedExpirationDate = document.createElement("span");
+        const renderedQuantity = document.createElement("span");
+        const deleteButtonContainer = document.createElement("span");
+        const deleteButton = document.createElement("button");
+
+        renderedProductName.textContent = tabMed.productName;
+        renderedProductType.textContent = tabMed.productType;
+        renderedTabletsDosageOption.textContent = tabMed.tabletsDosageOption;
+        renderedManufacturer.textContent = tabMed.manufacturer;
+        renderedExpirationDate.textContent = tabMed.expirationDate;
+        renderedQuantity.textContent = tabMed.quantity;
+        deleteButton.textContent = "Delete ❌";
+
+        tabletMedsUl.append(liRow);
+        liRow.append(
+          renderedProductName,
+          renderedProductType,
+          renderedTabletsDosageOption,
+          renderedManufacturer,
+          renderedExpirationDate,
+          renderedQuantity,
+          deleteButtonContainer
+        );
+        deleteButtonContainer.append(deleteButton);
       });
     }
   }
